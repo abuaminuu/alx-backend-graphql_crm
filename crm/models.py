a75f83d02517e91473c1c89cba5e1c129cc389e4
abuaminuu <i.abuaminu@gmail.com>
2025-12-12 11:12:17 +0100

    mod name in models

--- crm/models.py
+++ crm/models.py
@@ -5,7 +5,7 @@ import uuid
 # models here
 class Customer(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     phone = models.CharField(
         max_length=20,
